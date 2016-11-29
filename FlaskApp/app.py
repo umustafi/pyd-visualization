83a5e29e8b4bf54c7ebdad4788a926e265e7f410
Christina Curlette <cmcurlette@gmail.com>
2016-11-29 00:39:26 -0500

    Fiddling with port to try to get heroku hosting to work

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template
+import os
+
 app = Flask(__name__)
+
+port = int(os.environ.get('PORT', 33507))
 
 @app.route("/")
 def main():
     return render_template('index.html')
 
 if __name__ == "__main__":
-	app.run()
+	app.run(host='0.0.0.0', port=port)
