776ea7dcd20642b18b1d252a3e5a16c1abda1c3d
Christina Curlette <cmcurlette@gmail.com>
2017-01-16 01:15:53 -0500

    Fix #1.

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,13 +1,41 @@
-from flask import Flask, render_template
 import os
+import glob
+from flask import Flask, request, redirect, url_for
+from werkzeug import secure_filename
+
+UPLOAD_FOLDER = './tmp/'
+ALLOWED_EXTENSIONS = set(['csv', 'xls', 'xlsx'])
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-port = int(os.environ.get('PORT', 33507))
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-@app.route("/")
-def main():
-    return render_template('index.html')
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        files = glob.glob(app.config['UPLOAD_FOLDER'] + '*')
+        for f in files:
+            os.remove(f)
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('index'))
+    return """
+    <!doctype html>
+    <title><center>Data Visualization with Google Maps</title>
+    <link href="./static/bootstrap.min.css" rel="stylesheet" media="screen">
+    <h1><center>Data Visualization with Google Maps</h1>
+    <h4><center>Upload new file</h4>
+    <form action="" method=post enctype=multipart/form-data><center>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    <p><center>%s</p>
+    """ % "<br>".join(os.listdir(app.config['UPLOAD_FOLDER'],))
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=5000, debug=True)
