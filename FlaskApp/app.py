92d9d2108656e09e34579528fb4a748047d84add
Christina Curlette <cmcurlette@gmail.com>
2017-01-16 02:08:59 -0500

    Fix #9.

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,7 +1,14 @@
 import os
 import glob
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request, redirect, url_for, render_template
 from werkzeug import secure_filename
+
+import numpy as np
+import pandas as pd
+import gmplot
+from colour import Color
+from visual import process_data
+import geocoder
 
 UPLOAD_FOLDER = './tmp/'
 ALLOWED_EXTENSIONS = set(['csv', 'xls', 'xlsx'])
@@ -26,7 +33,7 @@ def index():
             return redirect(url_for('index'))
     return """
     <!doctype html>
-    <title><center>Data Visualization with Google Maps</title>
+    <title>Data Visualization with Google Maps</title>
     <link href="./static/bootstrap.min.css" rel="stylesheet" media="screen">
     <h1><center>Data Visualization with Google Maps</h1>
     <h4><center>Upload new file</h4>
@@ -35,7 +42,60 @@ def index():
          <input type=submit value=Upload>
     </form>
     <p><center>%s</p>
+    <form action="/render_map" method=post>
+         <input type=submit value="Render map">
+    </form>
     """ % "<br>".join(os.listdir(app.config['UPLOAD_FOLDER'],))
+
+@app.route('/render_map', methods=['GET', 'POST'])
+def render_map():
+    if request.method == 'POST':
+        file = glob.glob(app.config['UPLOAD_FOLDER'] + '*')[0]
+        print file
+
+        ZOOM_SIZE = 11
+        SIZE_CONSTANT = 20
+        ADDRESS_AFFIX = ", MA"
+
+        def location(city):
+            google = geocoder.google(city).latlng
+            if len(google) > 0:
+                return google
+            else:
+                o = geocoder.osm(city)
+                return [o.osm["y"], o.osm["x"]]
+
+        def map_data(wait_avg):
+            # convert dict to list of tuples sorted on avg wait time
+            wait_avg = sorted(wait_avg.items(), key=lambda x: x[1][0])
+            cities = [w[0] + ADDRESS_AFFIX for w in wait_avg]
+            locs = [location(x) for x in cities]
+
+            lats = [loc[0] for loc in locs]
+            lngs = [loc[1] for loc in locs]
+
+            lat_center = sum(lats) / len(lats)
+            lng_center = sum(lngs) / len(lngs)
+
+            zoom_size = ZOOM_SIZE
+
+            gmap = gmplot.GoogleMapPlotter(lat_center, lng_center, zoom_size)
+
+            green = Color("green")
+            colors = list(green.range_to(Color("red"), len(wait_avg)))
+            colors = [c.hex for c in colors]
+
+            sizes = [SIZE_CONSTANT * w[1][1] for w in wait_avg] # based on number of mentees
+
+            for i in xrange(len(lats)):
+                gmap.scatter([lats[i]], [lngs[i]], colors[i], size=sizes[i], marker=False)
+
+            gmap.draw("./templates/city_wait.html")
+
+        wait_avg = process_data(file)
+        map_data(wait_avg)
+        return render_template('city_wait.html')
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
